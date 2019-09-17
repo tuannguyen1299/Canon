a1e526b8c605889a273b780307456054ff5c023a
tuannguyen1299 <tuannguyennst99@gmail.com>
2019-09-17 11:09:38 +0700

    re

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get 'sesions/new'
+  # root 'application#hello'
+  root 'static_pages#home'
+  get '/help' , to: 'static_pages#help'
+  get '/about' , to: 'static_pages#about' 
+  get  '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+  resources :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+   
